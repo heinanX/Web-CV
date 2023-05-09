c89b839bb325d0da36dc1d6fc31c25f96d9ae219
heinanX <larrii@live.se>
2023-05-09 22:35:11 +0200

    added info and styling to quali and proj

--- public/scripts/letter.js
+++ public/scripts/letter.js
@@ -1,42 +1,73 @@
+const funfact_div = document.querySelector('#funFact');
+const a = document.createElement('a')
+const p = document.createElement('p')
+p.setAttribute('class', 'd-inline')
+a.setAttribute('class', 'ps-2')
+a.setAttribute('href', 'http://eskilssonlinda.myportfolio.com')
+a.setAttribute('onclick', `window.open(this.href,'_blank');return false;`)
+
+const init = () => {
+    funfact_div.addEventListener('mouseenter', getLink);
+    funfact_div.addEventListener('mouseleave', removeLink);
+    
+    renderLetter();
+}
+
+
 const translate = () => {
     const translateBtn = document.querySelector('.translate');
     translateBtn.removeEventListener('click', translate);
     translateBtn.addEventListener('click', renderLetter);
+    
+    translateBtn.textContent = 'English?'
 
     const getVisitor = localStorage.getItem('visitor')
     const para1 = document.querySelector('#paragraph-1');
-    const para2 = document.querySelector('#paragraph-2');
-    const para3 = document.querySelector('#paragraph-3');
-    const para4 = document.querySelector('#paragraph-4');
-
-    para1.textContent = `Hej igen, ${getVisitor}!`
-    para2.textContent = `Kul att du hittade hit. Eftersom jag redan har introducerat mig sj'lv skippar vi den biten.`
-    para3.textContent = `I'm a!  Som studerande webbutvecklare har jag erfarenhet av både frontend och backend
-    programmering, där vi främst fokuserat på Javascipt och node.js som backendspråk.
-    Utöver det har jag förståelse för PHP och goda erfarenhet av HTML och CSS. Jag ställer
-    alltid höga krav på mitt arbete och tar stor stolthet i det jag gör.`
-    para4.textContent = `Som studerande webbutvecklare har jag erfarenhet av både frontend och backend
-    programmering, där vi främst fokuserat på Javascipt och node.js som backendspråk.
-    Utöver det har jag förståelse för PHP och goda erfarenhet av HTML och CSS. Jag ställer
-    alltid höga krav på mitt arbete och tar stor stolthet i det jag gör.!`
-
-}
-
-const renderLetter = () => {
-    const translateBtn = document.querySelector('.translate');
-    translateBtn.removeEventListener('click', renderLetter);
-    translateBtn.addEventListener('click', translate);
-    translateBtn.textContent = 'English'
-
-    const getVisitor = localStorage.getItem('visitor')
-    const para1 = document.querySelector('#paragraph-1');
+    const lsName = document.querySelector('#ls-name');
     const para2 = document.querySelector('#paragraph-2');
     const para3 = document.querySelector('#paragraph-3');
     const para4 = document.querySelector('#paragraph-4');
     const para5 = document.querySelector('#paragraph-5');
     const para6 = document.querySelector('#paragraph-6');
+    const para7 = document.querySelector('#paragraph-7');
 
-    para1.textContent = `Hi, ${getVisitor}!`
+    para1.textContent = `Hej igen,`
+    lsName.textContent = `${getVisitor}!`
+    para2.textContent = `Kul att se att du hittade hit. Låt mig presentera mig själv mer ingående den här gången. Som ni redan vet är namnet Linda och jag är student på Medieinstitutet i Stockholm där jag studerar Webbprogrammering för e-handel. Det betyder att jag har erfarenhet av både Backend och Frontend programmering.`
+
+    para3.textContent = `I skolan har vi fokuserat främst på att använda Javascript eftersom det är både ett frontend- och backendspråk (Node.js). Utöver det har jag förståelse för PHP och goda erfarenhet av Transcript, HTML och CSS.`
+
+    para4.textContent = `Som student har jag bl.a. lärt mig att bygga webbsidor från början till slut och som person är jag van att ta eget initiativ, att finna och driva mina egna uppgifter, så mellan mina skoluppgifter tycker jag om att arbeta på mindre projekt och har till och med skapat mitt eget Yahtzee.`
+
+    para5.textContent = `Det jag gillar mest med programmering är att det alltid utmanar mig. Det är som en skattjakt: en resa full av prövningar och (ibland så frustrerande!) misstag, där vinsten är stoltheten och nöjet av ännu ett avslutat projekt.`
+
+    para6.innerHTML = `Jag ställer alltid höga krav på mitt arbete och tar stolthet i det jag levererar. Jag trivs att arbeta i team samtidigt som jag inte drar mig för att arbeta ensam. Nyfiken är mitt mellannamn och jag är inte rädd för att söka svar när jag behöver dem. Kort sagt, är jag övertygad om att min erfarenhet och attityd gör mig till en värdefull teamplayer för många arbetsteam.<br><br>Om du letar efter en ivrig praktikant, eller bara vill connecta, skicka ett <a href="mailto:lindaEskilsson@medieInstitutet.se">meddelande</a>. Tills vi ses igen!`
+
+    para7.innerHTML = `Vänliga hälsningar, <br> Linda`
+
+}
+
+const renderLetter = () => {
+
+    /* LETTER  TEXT CONTENT*/
+    const translateBtn = document.querySelector('.translate');
+    translateBtn.removeEventListener('click', renderLetter);
+    translateBtn.addEventListener('click', translate);
+
+    translateBtn.innerHTML = 'Svenska?';
+
+    const getVisitor = localStorage.getItem('visitor');
+    const para1 = document.querySelector('#paragraph-1');
+    const lsName = document.querySelector('#ls-name');
+    const para2 = document.querySelector('#paragraph-2');
+    const para3 = document.querySelector('#paragraph-3');
+    const para4 = document.querySelector('#paragraph-4');
+    const para5 = document.querySelector('#paragraph-5');
+    const para6 = document.querySelector('#paragraph-6');
+    const para7 = document.querySelector('#paragraph-7');
+
+    para1.textContent = `Hi again, `
+    lsName.textContent = `${getVisitor}!`
     
     para2.textContent = `Glad to see you found your way back here. Let me introduce myself more thoroughly this time. As you already know, the name is Linda and I'm a student of Medieinstitutet in Stockholm where I study Web Programming for eCommerce. This means I have experience with both Backend and Frontend programming.`
 
@@ -44,9 +75,28 @@ const renderLetter = () => {
     
     para4.textContent = `As a student I've learned to build webpages from start to finish, though do not limit myself to ecommerce stores only. In between school assignments, I enjoy working on smaller projects and have even created my own Yahtzee. `
 
-    para5.textContent = `What I like the most about programming is that it always challenges me to think. It is like its own little treasure hunt: a journey full of trials and (at times, so frustrating) errors. Though in the end, as each element is implemented, you get closer to the finish line where the end prize is the satisfaction of another completed project.`
+    para5.textContent = `What I like the most about programming is that it always challenges me to think. It is like its own little treasure hunt: a journey full of trials and (at times, so frustrating!) errors. Though in the end, as each element is implemented, you get closer to the finish line where the end prize is the satisfaction of another completed project.`
 
-    para6.textContent = `I put high expectations on all my work and take pride in what I deliver.`
+    para6.innerHTML = `By nature I hold myself to high standards in all the things I do and take pride in what I deliver. I enjoy learning and am always willing to seek out answers when I need them. In short, I am confident that my experience and attitude makes me a valuable asset to any team.<br><br>If you're looking for an eager intern, or just want to connect, send me a <a href="mailto:lindaEskilsson@medieInstitutet.se">hello</a>.<br> Until we meet again!`
+
+    para7.innerHTML = ` Sincerely, <br> Linda`
+
+    /* COLUMN C TEXT CONTENT */
+    
 }
-renderLetter();
 
+
+const getLink = () => {
+    p.textContent = `Check it out if you got the time!`
+    a.textContent = `>> Here <<`
+    funfact_div.append(p, a)
+    
+}
+const removeLink = () => {
+    a.innerText = '';
+    p.innerText = ''
+
+}
+
+
+init()
---
+++ public/scripts/projects.js
@@ -0,0 +1,69 @@
+const blockB = document.querySelector('.blockB')
+const listItems = document.querySelectorAll('.sidemenu > li')
+listItems.forEach(element => {
+    element.addEventListener('click', () => { printData(element)})
+})
+
+// declares an empty variable
+let jsonData;
+
+// function that fetches data from JSON and stores it in variable 'data'
+const readJson = async () => {
+    const response = await fetch ('../json_data/projects.json');
+    jsonData = await response.json()
+    loopItems(jsonData)
+}
+
+const printData = (element) => {
+    const savedID = element.getAttribute('id')
+    let projects;
+    switch(savedID){
+        case 'myProjects':
+            projects = jsonData.filter((project) => project.collab == false)
+            loopItems(projects);
+
+        break;
+        case 'collaborations':
+            projects = jsonData.filter((project) => project.collab == true)
+            loopItems(projects);
+        break;
+        case 'frontend':
+            projects = jsonData.filter((project) => project.type == 'frontend')
+            console.log(projects);
+            loopItems(projects);
+        break;
+        case 'backend':
+            projects = jsonData.filter((project) => project.type == 'backend')
+            console.log(projects);
+            loopItems(projects);
+        break;
+        default:
+            loopItems(jsonData)
+        
+    }
+}
+
+const loopItems = (items) => {
+    blockB.textContent = '';
+    for (const item of items) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'center--adjust')
+        const img = document.createElement('img');
+        const h4 = document.createElement('h4');
+        const a = document.createElement('a');
+        a.setAttribute('onclick', `window.open(this.href,'_blank');return false;`)
+        const p = document.createElement('p');
+
+        a.setAttribute('href', item.link)
+        h4.textContent = item.title;
+        p.textContent = item.description;
+
+        a.append(h4)
+        div.append(a, p);
+        blockB.append(div)
+    }
+}
+
+
+
+readJson()
